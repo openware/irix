2dd59cbcddac9521960c757237366aaac13bdc7e
Moch Gufron E <mgufronefendi@gmail.com>
2021-05-07 19:21:16 +0700

    keep go vet

--- run_tests.sh
+++ run_tests.sh
@@ -4,19 +4,17 @@ set -xe
 ROOT_DIR=$PWD
 
 go mod vendor
+go vet ./...
 go test .
 for d in */ ; do
   if [ $d = "huobi/" ] || [ $d = "vendor/" ]; then
     continue
   fi
 	cd $d
-    if [ -f "$d/go.mod" ]; then
-
-	go mod download
-
-    fi
+  if [ -f "$d/go.mod" ]; then
+    go mod download
+  fi
   go test -cover .
 	cd $ROOT_DIR
 done
 
-# ignore huobi for now. as the test calls directly to the api and some of them are outdated
